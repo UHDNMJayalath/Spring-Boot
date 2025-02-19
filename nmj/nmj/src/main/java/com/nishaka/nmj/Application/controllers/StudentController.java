cf0746e12b15475e13bb0c980de4f3f82c6b8796
UHDNMJayalath <nishakamahesh2020@gmail.com>
2025-02-20 02:36:32 +0530

    Updated StudentController and StudentRepository

--- nmj/nmj/src/main/java/com/nishaka/nmj/Application/controllers/StudentController.java
+++ nmj/nmj/src/main/java/com/nishaka/nmj/Application/controllers/StudentController.java
@@ -1,17 +1,25 @@
 package com.nishaka.nmj.Application.controllers;
 
+import com.nishaka.nmj.Domain.entity.Student;
+import com.nishaka.nmj.External.repository.StudentRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/student")
+@AllArgsConstructor
 public class StudentController {
 
+    private final StudentRepository studentRepository;
     @GetMapping("/list")
-    public String getStudent() {
-        return "Student List";
+    public List<Student> getStudent() {
+        return studentRepository.findAll();
+
     }
 
     @PostMapping("/add")
--- nmj/nmj/src/main/java/com/nishaka/nmj/External/repository/StudentRepository.java
+++ nmj/nmj/src/main/java/com/nishaka/nmj/External/repository/StudentRepository.java
@@ -1,2 +1,11 @@
-package com.nishaka.nmj.External.repository;public interface StudentRepository {
+package com.nishaka.nmj.External.repository;
+
+
+import com.nishaka.nmj.Domain.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository <Student ,Integer> {
+
 }
