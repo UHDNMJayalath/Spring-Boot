245cf14e22e4a3445ad4975b881a256273d1be23
UHDNMJayalath <nishakamahesh2020@gmail.com>
2025-02-20 02:34:22 +0530

    nmj/nmj/src/main/java/com/nishaka/nmj/Domain/entity/Student.java

--- nmj/nmj/src/main/java/com/nishaka/nmj/Domain/entity/Student.java
+++ nmj/nmj/src/main/java/com/nishaka/nmj/Domain/entity/Student.java
@@ -1,8 +1,64 @@
 package com.nishaka.nmj.Domain.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 @Entity
 public class Student {
-    private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String address;
+    private String email;
+    private Integer grade;
+
+    public Student(Integer id, String name, String address, String email, Integer grade) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.email = email;
+        this.grade = grade;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Integer grade) {
+        this.grade = grade;
+    }
 }
