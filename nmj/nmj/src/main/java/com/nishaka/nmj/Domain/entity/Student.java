e69b9059eb6d7b6a25e9290fca723ac99fdc3769
UHDNMJayalath <nishakamahesh2020@gmail.com>
2025-02-20 01:11:23 +0530

    nmj/nmj/src/main/java/com/nishaka/nmj/Domain/entity/Student.java

--- nmj/nmj/src/main/java/com/nishaka/nmj/Domain/entity/Student.java
+++ nmj/nmj/src/main/java/com/nishaka/nmj/Domain/entity/Student.java
@@ -1,2 +1,8 @@
-package com.nishaka.nmj.Domain.entity;public class Student {
+package com.nishaka.nmj.Domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+@Entity
+public class Student {
+    private Long id;
 }
