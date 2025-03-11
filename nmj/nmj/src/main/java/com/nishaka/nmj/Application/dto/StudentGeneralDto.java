69cbc2c2e2c72605694d12363396f6ec1f5d42b0
UHDNMJayalath <nishakamahesh2020@gmail.com>
2025-03-11 20:30:29 +0530

    nmj/nmj/src/main/java/com/nishaka/nmj/Application/dto/StudentGeneralDto.java

--- nmj/nmj/src/main/java/com/nishaka/nmj/Application/dto/StudentGeneralDto.java
+++ nmj/nmj/src/main/java/com/nishaka/nmj/Application/dto/StudentGeneralDto.java
@@ -1,5 +1,6 @@
 package com.nishaka.nmj.Application.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
@@ -8,4 +9,27 @@ public class StudentGeneralDto {
     private String name;
     private Integer grade;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Integer grade) {
+        this.grade = grade;
+    }
 }
